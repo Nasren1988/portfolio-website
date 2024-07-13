9fd12c4116774ac7485695d0d74e16b0f9a668ae
Nasren-khazaei <gonjishk90@gmail.com>
2024-07-13 17:18:02 +0330

    Initial commit: Created portfolio website

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add JavaScript to toggle the menu
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navMenu = document.getElementById('nav-menu');
+
+    menuToggle.addEventListener('click', function() {
+        navMenu.classList.toggle('show');
+    });
+});
